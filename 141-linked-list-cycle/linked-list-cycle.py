409a444cb428dc72c28bedaf9916140184e894ca
GabrielTrevisoliRodrigues <gabrieltrevisoliprofissional@gmail.com>
2025-10-15 21:10:54 -0300

    Time: 37 ms (57.52%) | Memory: 20.1 MB (11.87%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        visited = set()    # 1
-        node = head        # 2
-        while node:        # 3
-            if node in visited:   # 4
-                return True       # 5
-            visited.add(node)     # 6
-            node = node.next      # 7
-        return False              # 8
+        visited = set()    
+        node = head        
+        while node:        
+            if node in visited:   
+                return True       
+            visited.add(node)     
+            node = node.next      
+        return False              
